b0214a6eda5ad1a1dffd728d360156929b5f8cd2
Arjun Kalburgi <askalburgi@gmail.com>
2018-02-24 13:02:51 -0700

    cleanup

--- linearsystemsolver.c
+++ linearsystemsolver.c
@@ -25,53 +25,65 @@ void print_usage() {
 }
 void solve_linear_systems_of_equations() {
     GET_TIME(start);
+
     // BASELINE SOLUTION: 
     // NEED TO ENSURE OPENMP WORKS FIRST
 
-//     if (size == 1)
-//         X[0] = Au[0][1] / Au[0][0];
-//     else
-//     {
-//         /*Gaussian elimination*/
-//         for (k = 0; k < size - 1; ++k)
-//         {
-//             /*Pivoting*/
-//             int temp = 0;
-//             for (i = k, j = 0; i < size; ++i)
-//                 if (temp < Au[indices[i]][k] * Au[indices[i]][k])
-//                 {
-//                     temp = Au[indices[i]][k] * Au[indices[i]][k];
-//                     j = i;
-//                 }
-//             if (j != k) /*swap*/
-//             {
-//                 i = indices[j];
-//                 indices[j] = indices[k];
-//                 indices[k] = i;
-//             }
-//                 /*calculating*/
-//            #pragma omp parallel for num_threads(thread_count) default(none) shared(Au, indices, k, size) private(i, j, temp)
-//             for (i = k + 1; i < size; ++i)
-//             {
-//                 temp = Au[indices[i]][k] / Au[indices[k]][k];
-//                 for (j = k; j < size + 1; ++j)
-//                     Au[indices[i]][j] -= Au[indices[k]][j] * temp;
-//             }
-//         }
-//         /*Jordan elimination*/
-//         for (k = size - 1; k > 0; --k)
-//         {
-//             for (i = k - 1; i >= 0; --i)
-//             {
-//                 temp = Au[indices[i]][k] / Au[indices[k]][k];
-//                 Au[indices[i]][k] -= temp * Au[indices[k]][k];
-//                 Au[indices[i]][size] -= temp * Au[indices[k]][size];
-//             }
-//         }
-//         /*solution*/
-//         for (k = 0; k < size; ++k)
-//             X[k] = Au[indices[k]][size] / Au[indices[k]][k];
-//     }
+    // if (size == 1)
+    //     X[0] = Au[0][1] / Au[0][0];
+    // else{
+    //     /*Gaussian elimination*/
+    // 	for (int k = 0; k < size - 1; ++k) {
+		
+	// 	    /*Pivoting*/
+	// 	    int temp = 0;
+	// 	    int j = 0;
+	// 	    # pragma omp parallel for private(i)
+	// 	    for (int i = k; i < size; ++i) {
+	// 	       if (temp < Au[indices[i]][k] * Au[indices[i]][k]){
+    //                  # pragma omp critical
+	//                  { 
+	//                     if (temp < Au[indices[i]][k] * Au[indices[i]][k]){
+	//                         temp = Au[indices[i]][k] * Au[indices[i]][k];
+	//                         j = i;
+	//                     }
+	//                  }
+	// 			}
+    //         }
+
+
+	// 	    if (j != k)/*swap*/{
+	// 			int i = indices[j];
+	// 			indices[j] = indices[k];
+	// 			indices[k] = i;
+	// 	    }
+		    
+	// 	    /*calculating*/
+	// 	    #pragma omp parallel for private(i,temp,j)
+	// 	    for (int i = k + 1; i < size; ++i){
+	// 	        temp = Au[indices[i]][k] / Au[indices[k]][k];
+	// 	        for (j = k; j < size + 1; ++j) {
+	// 	            Au[indices[i]][j] -= Au[indices[k]][j] * temp;
+	// 			}
+	// 	    }   
+    // 	}
+    //     /*Jordan elimination*/
+	//     for (int k = size - 1; k > 0; --k){
+	//      #pragma omp parallel for private(temp)	
+	//         for (int i = k - 1; i >= 0; --i ){
+	//             temp = Au[indices[i]][k] / Au[indices[k]][k];
+	//             Au[indices[i]][k] -= temp * Au[indices[k]][k];
+	//             Au[indices[i]][size] -= temp * Au[indices[k]][size];
+	//         } 
+	//     }
+	//         /*solution*/
+	// 	#pragma omp parallel for
+	//         for (int k=0; k< size; ++k) {
+	//             X[k] = Au[indices[k]][size] / Au[indices[k]][k];
+	// 	    printf("%e\n", X[k]);
+	// 	}
+    // }
+
     GET_TIME(end);
 }
 
