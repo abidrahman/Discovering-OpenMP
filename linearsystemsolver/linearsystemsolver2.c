16ccf3ec582f1f81ce0d33289d5685e330a351ac
Arjun Kalburgi <askalburgi@gmail.com>
2018-02-26 16:19:01 -0700

    working so far haha

--- linearsystemsolver/linearsystemsolver2.c
+++ linearsystemsolver/linearsystemsolver2.c
@@ -1,34 +1,32 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <omp.h>
-#include "./../dependencies/Lab3IO.h"
-#include "./../dependencies/timer.h"
+#include "linearsystemsolver.h"
 
 int main(int argc, char *argv[])
 {
-    int i, j, k, size;
-    int thread_count;
-    double **Au;
-    double *X;
-    int *index;
-    double temp;
-    double start, end;
-
-    if (argc <= 1)
-    {
-        printf("Insufficient input!\n");
-        return 1;
-    }
-    thread_count = strtol(argv[1], NULL, 10);
+    thread_count = 2; 
 
     /*Calculate the solution by serial code*/
     Lab3LoadInput(&Au, &size);
     X = CreateVec(size);
-    index = malloc(size * sizeof(int));
-    for (i = 0; i < size; ++i)
-        index[i] = i;
+    indices = malloc(size * sizeof(int));
+    for (int i = 0; i < size; ++i)
+        indices[i] = i;
 
     GET_TIME(start);
+    solve_linear_systems_of_equations(); 
+    GET_TIME(end);
+    Lab3SaveOutput(X, size, end - start);
+
+    DestroyVec(X);
+    DestroyMat(Au, size);
+    free(indices);
+    return 0;
+}
+
+void solve_linear_systems_of_equations()
+{
+    int i, j, k;
+    double temp;
+
     if (size == 1)
         X[0] = Au[0][1] / Au[0][0];
     else
@@ -39,24 +37,24 @@ int main(int argc, char *argv[])
             /*Pivoting*/
             temp = 0;
             for (i = k, j = 0; i < size; ++i)
-                if (temp < Au[index[i]][k] * Au[index[i]][k])
+                if (temp < Au[indices[i]][k] * Au[indices[i]][k])
                 {
-                    temp = Au[index[i]][k] * Au[index[i]][k];
+                    temp = Au[indices[i]][k] * Au[indices[i]][k];
                     j = i;
                 }
             if (j != k) /*swap*/
             {
-                i = index[j];
-                index[j] = index[k];
-                index[k] = i;
+                i = indices[j];
+                indices[j] = indices[k];
+                indices[k] = i;
             }
                 /*calculating*/
-#pragma omp parallel for num_threads(thread_count) default(none) shared(Au, index, k, size) private(i, j, temp)
+#pragma omp parallel for num_threads(thread_count) default(none) shared(Au, indices, k, size) private(i, j, temp)
             for (i = k + 1; i < size; ++i)
             {
-                temp = Au[index[i]][k] / Au[index[k]][k];
+                temp = Au[indices[i]][k] / Au[indices[k]][k];
                 for (j = k; j < size + 1; ++j)
-                    Au[index[i]][j] -= Au[index[k]][j] * temp;
+                    Au[indices[i]][j] -= Au[indices[k]][j] * temp;
             }
         }
         /*Jordan elimination*/
@@ -64,20 +62,13 @@ int main(int argc, char *argv[])
         {
             for (i = k - 1; i >= 0; --i)
             {
-                temp = Au[index[i]][k] / Au[index[k]][k];
-                Au[index[i]][k] -= temp * Au[index[k]][k];
-                Au[index[i]][size] -= temp * Au[index[k]][size];
+                temp = Au[indices[i]][k] / Au[indices[k]][k];
+                Au[indices[i]][k] -= temp * Au[indices[k]][k];
+                Au[indices[i]][size] -= temp * Au[indices[k]][size];
             }
         }
         /*solution*/
         for (k = 0; k < size; ++k)
-            X[k] = Au[index[k]][size] / Au[index[k]][k];
+            X[k] = Au[indices[k]][size] / Au[indices[k]][k];
     }
-    GET_TIME(end);
-    Lab3SaveOutput(X, size, end - start);
-
-    DestroyVec(X);
-    DestroyMat(Au, size);
-    free(index);
-    return 0;
 }
